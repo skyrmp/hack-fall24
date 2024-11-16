783d6fafeadafcb600705fa4c19c979e32fca78e
mazmystic <mllabowitz@gmail.com>
2024-11-16 18:57:41 -0500

    Update mood_alert.gd

--- scenes/mood_alert/mood_alert.gd
+++ scenes/mood_alert/mood_alert.gd
@@ -3,7 +3,7 @@ extends Node2D
 
 func set_ratio(ratio: float) -> void:
 	var amt: int = $Sprite.hframes
-	set_level(round(remap(ratio, 0.0, 1.0, amt - 1, 0))
+	set_level(round(remap(ratio, 0.0, 1.0, amt - 1, 0)))
 
 
 func set_level(level: int) -> void:
