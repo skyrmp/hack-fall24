41fcbdb00bc7d672b41d466eace58a0c4ff8f1da
Adam Kleber <adam.kleber@gmail.com>
2024-11-16 15:51:38 -0500

    started working on critter

--- scenes/critter/critter.gd
+++ scenes/critter/critter.gd
@@ -1,10 +1,22 @@
+
+extends Area2D
+
 class_name Critter
 
 var mental_hp: float
 var emo_resist: int
 
-var seed_drop_chance: int
+var speed: float
+var mhp_speed_ratio: float
+
+#var seed_drop_chance: int
 var possible_seeds: Array[int]
+
+func take_damage(damage: float) -> void:
+	mental_hp -= damage / emo_resist
+	speed = mental_hp * mhp_speed_ratio
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,6 +25,3 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-func damage(damage: float) -> void:
-	mental_hp -= damage / emo_resist
