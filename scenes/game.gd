ed831a032686b79453ae2dcba77ce3e34c578a4c
webcatzz <june.webcatz@gmail.com>
2024-11-16 16:55:45 -0500

    removed old critter

--- scenes/game.gd
+++ scenes/game.gd
@@ -1,1 +1,7 @@
 extends Node2D
+
+
+func add_plant(plant_name: String, pos: Vector2):
+	var plant: Plant = preload("res://scenes/plant.tscn").instantiate()
+	plant.data = load("res://resources/plants/%s.tres" % plant_name)
+	plant.position = pos
