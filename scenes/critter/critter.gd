d8ad6c936f2b3f33ad39f9005bef3b534706e768
mazmystic <mllabowitz@gmail.com>
2024-11-16 18:15:40 -0500

    Add Critter things

--- scenes/critter/critter.gd
+++ scenes/critter/critter.gd
@@ -1,25 +1,126 @@
 class_name Critter
 extends Area2D
 
-var max_hp: int
-var current_hp: float = max_hp
+enum {MOVING, VISITING, LEAVING, SCARED}
 
-var speed: float
-var hp_speed_ratio: float
+@export var critter_data: CritterData
 
-#var seed_drop_chance: int
-var possible_seeds: Array[int]
+@export var hp: float
+@export var speed: float
 
-func take_damage(damage: float) -> void:
-	current_hp -= damage
-	speed = current_hp * hp_speed_ratio
+var state: int:
+	set(value):
+		if value == LEAVING or value == SCARED:
+			_pick_target_edge_point()
+		elif value == VISITING:
+			_visit_ratio = 0.0
+			_visit_duration = randf_range(critter_data.visit_wait_range[0], critter_data.visit_wait_range[1])
+		
+		state = value
+
+var target_plot: Node2D
+
+var _target_edge_point: Vector2
+var _visit_ratio: float = 0.0:
+	set(value):
+		_visit_ratio = clampf(value, 0.0, 1.0)
+
+var _visit_duration: float
+var _plots_to_visit: int
+var _plots_visited: int = 0
 
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$VisibleOnScreenNotifier2D.screen_exited.connect(queue_free)
+	_setup_critter_data()
+	_pick_target_plot()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+
+func _physics_process(delta: float) -> void:
+	match state:
+		MOVING:
+			_move_towards_plot(delta)
+		VISITING:
+			_visit_plot(delta)
+		LEAVING:
+			_leave(delta)
+		SCARED:
+			_run_away(delta)
+
+
+func take_damage(damage: float = 1.0) -> void:
+	hp -= damage
+	speed = critter_data.base_speed * (hp / critter_data.max_hp)
+	if hp <= 0.0:
+		target_plot = null
+		state = SCARED
+
+
+func get_eat_ratio() -> float:
+	return _visit_ratio
+
+
+func _move_towards_plot(delta: float) -> void:
+	global_position = global_position.move_toward(target_plot.global_position, delta * speed)
+	if target_plot.global_position - global_position == Vector2.ZERO:
+		state = VISITING
+
+
+func _visit_plot(delta: float) -> void:
+	if critter_data.is_good:
+		_visit_ratio += (delta / _visit_duration)
+		
+		if _visit_ratio == 1.0:
+			_plots_visited += 1
+			if _plots_visited >= _plots_to_visit:
+				state = LEAVING
+			else:
+				_pick_target_plot()
+	
+	# Case for bad critter
+	else:
+		# If plant is dead or nonexistent, reselect
+		if target_plot.plant == null:
+			_pick_target_plot()
+			return
+		
+		_visit_ratio += (delta / critter_data.eat_speed) * (hp/critter_data.max_hp)
+		
+		if _visit_ratio == 1.0:
+			target_plot.plant.take_damage()
+
+
+func _leave(delta: float) -> void:
+	global_position = global_position.move_toward(target_plot.global_position, delta * critter_data.base_speed)
+
+
+func _run_away(delta: float) -> void:
+	global_position = global_position.move_toward(target_plot.global_position, delta * critter_data.scared_speed)
+
+
+func _setup_critter_data() -> void:
+	hp = critter_data.max_hp
+	speed = critter_data.base_speed
+	_plots_to_visit = randi_range(critter_data.visit_amount_range[0], critter_data.visit_amount_range[1])
+
+
+## Picks a plot to go to. Also sets state
+func _pick_target_plot() -> void:
+	var plots: Array = get_tree().get_nodes_in_group(&"plot")
+	
+	# Remove empty plots if critter is bad
+	if not critter_data.is_good:
+		for i in range(plots.size() - 1, -1, -1):
+			if plots[i].plant == null:
+				plots.remove_at(i)
+	
+	if plots.is_empty():
+		state = LEAVING
+	else:
+		target_plot = plots.pick_random()
+		state = MOVING
+
+
+func _pick_target_edge_point() -> void:
+	var screen_center: Vector2 = get_viewport_rect().size / 2.0
+	_target_edge_point = Vector2(global_position - screen_center).normalized() * (get_viewport_rect().size.x * 2.0)
---
+++ scenes/critter/critter_data/critter_data.gd
@@ -0,0 +1,17 @@
+class_name CritterData
+extends Resource
+
+@export var is_good: bool = false
+
+@export var max_hp: float = 1.0
+@export var base_speed: float = 100.0
+
+@export var scared_speed: float = 150.0
+
+@export var eat_speed: float = 1.0
+
+@export var visit_amount_range: Array[int] = [2, 5]
+@export var visit_wait_range: Array[float] = [0.5, 3.0]
+
+#@export var sprite_frames: SpriteFrames
+#@export var seeds: Array[Seed]
