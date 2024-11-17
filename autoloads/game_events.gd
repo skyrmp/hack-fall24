6c8733bfd44b4394021ed0c5dabf41fc68fa6362
mazmystic <mllabowitz@gmail.com>
2024-11-17 01:16:10 -0500

    Updated for game.gd

--- autoloads/game_events.gd
+++ autoloads/game_events.gd
@@ -2,3 +2,5 @@ extends Node
 
 
 signal plant_spawned(plot: Plot)
+
+signal critter_scared(critter: Critter)
--- scenes/critter/critter.gd
+++ scenes/critter/critter.gd
@@ -80,6 +80,8 @@ func _start_state(p_state: int) -> void:
 			pick_target_edge_point()
 			if is_good():
 				drop_seed()
+			
+			GameEvents.critter_scared.emit()
 
 
 func _physics_process(delta: float) -> void:
